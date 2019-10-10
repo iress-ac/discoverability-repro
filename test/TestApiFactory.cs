24255634f9809c8f70c82c0374e41c0cd043c4b0
Adam Chodera <adam.chodera@iress.co.uk>
2019-10-10 10:21:56 +0100

    use hostbuilder in api factory

--- test/TestApiFactory.cs
+++ test/TestApiFactory.cs
@@ -1,25 +1,24 @@
 ﻿using api;
-using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
-using Microsoft.AspNetCore.TestHost;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace test
 {
     public class TestApiFactory : WebApplicationFactory<TestStartup>
     {
-        protected override IWebHostBuilder CreateWebHostBuilder()
+        protected override IHostBuilder CreateHostBuilder()
         {
-            return WebHost.CreateDefaultBuilder()
-                .UseStartup<TestStartup>()
-                .ConfigureTestServices(services => services.AddControllers().AddApplicationPart(typeof(Startup).Assembly));
-        }
-
-        protected override void ConfigureWebHost(IWebHostBuilder builder)
-        {
-            builder.UseContentRoot(".");
-            base.ConfigureWebHost(builder);
+            return Host.CreateDefaultBuilder()
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                })
+                .ConfigureServices(services =>
+                {
+                    services.AddControllers().AddApplicationPart(typeof(Startup).Assembly);
+                });
         }
     }
 }
